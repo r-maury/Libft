43b7ce7d1065d6368a43189be98f7ee9b2332581
Romain Maury <rmaury@student.42.fr>
2015-04-22 15:04:48 +0200

    norme ok

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: rmaury <rmaury@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/07 16:26:53 by rmaury            #+#    #+#             */
-/*   Updated: 2015/03/24 18:41:48 by rmaury           ###   ########.fr       */
+/*   Updated: 2015/04/22 15:03:42 by rmaury           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 	str = (char*)s1;
 	str2 = (char*)s2;
 	if (s2[0] == '\0')
-		return(str);
+		return (str);
 	l = ft_strlen(s2);
-	while(str[i] && i <= (int)n - l)
+	while (str[i] && i <= (int)n - l)
 	{
 		if (ft_strncmp(&str[i], str2, l) == 0)
 			return (&str[i]);
